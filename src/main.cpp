bea6cba7bc0626457cffb824a68e13e791fb6f4e
Aru <albinkun@gmail.com>
2010-12-10 18:28:35 -0600

    did some random changes?

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,35 @@
 #include <osgViewer/Viewer>
 #include <osg/PositionAttitudeTransform>
 #include <osgGA/TrackballManipulator>
+#include "MouseEventHandler.h"
 
-int main()
+int main( int argc, char** argv )
 {
+
+	/* Timer */
+	static osg::Timer_t old_tick, new_tick; 
+	static double delta;
+
+	/* Initialize viewer */
     osgViewer::Viewer viewer;
+
+	// Rest of the code goes here
+
+	/* Realize the viewer */
     viewer.realize();
 
     while( !viewer.done() )
     {
+
+		/* tick, tick, tick... */
+		new_tick = osg::Timer::instance()->tick();
+        delta = osg::Timer::instance()->delta_s( old_tick, new_tick );
+
+		/* frame */
         viewer.frame();
+
+		/* Actualizacion del tick */
+		old_tick = new_tick;
     }
 
     return 0;
